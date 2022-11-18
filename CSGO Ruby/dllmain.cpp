5cc5e13be358b048a893792d88f3a5e4f4dfe1d2
Tamir-M <tamirmousari@gmail.com>
2022-11-18 10:54:48 +0200

    Add project files.

---
+++ CSGO Ruby/dllmain.cpp
@@ -0,0 +1,33 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <Windows.h>
+#define PANIC_KEY VK_END
+
+
+WORD WINAPI HackThread(HMODULE hModule) {
+
+    while (!GetAsyncKeyState(PANIC_KEY)) {
+
+    }
+
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: {
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
