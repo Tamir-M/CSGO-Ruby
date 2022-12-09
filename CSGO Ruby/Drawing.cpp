62677da7ee0c9eb81b185efb18350b88b157bd60
Tamir-M <tamirmousari@gmail.com>
2022-12-09 20:05:11 +0200

    Added Text to drawing so I can write text woohoo

--- CSGO Ruby/Drawing.cpp
+++ CSGO Ruby/Drawing.cpp
@@ -1,4 +1,5 @@
 #include "Drawing.h"
+#include "HackFeatures.h"
 
 ID3DXLine* LineL;
 
@@ -40,4 +41,78 @@ void DrawESPBox2D(Vec2 top, Vec2 bot, FLOAT thickness, D3DCOLOR color) {
 	DrawLine(bl, br, thickness, color);
 	DrawLine(tl, bl, thickness, color);
 	DrawLine(tr, br, thickness, color);
+}
+
+void DrawESPBox3D(Vec3 top, Vec3 bot, FLOAT a, int width, FLOAT thickness, D3DCOLOR color) {
+	float height3D = ABS(top.z - bot.z);
+	Vec3 b1, b2, b3, b4, t1, t2, t3, t4;
+	b1.z = b2.z = b3.z = b4.z = bot.z;
+
+	b1.x = bot.x + (cosf(TORAD(a + 45)) * width);
+	b1.y = bot.y + (sinf(TORAD(a + 45)) * width);
+	
+	b2.x = bot.x + (cosf(TORAD(a + 135)) * width);
+	b2.y = bot.y + (sinf(TORAD(a + 135)) * width);
+	
+	b3.x = bot.x + (cosf(TORAD(a + 225)) * width);
+	b3.y = bot.y + (sinf(TORAD(a + 225)) * width);
+	
+	b4.x = bot.x + (cosf(TORAD(a + 315)) * width);
+	b4.y = bot.y + (sinf(TORAD(a + 315)) * width);
+
+	t1.x = b1.x;
+	t1.y = b1.y;
+	t1.z = b1.z + height3D;
+	
+	t2.x = b2.x;
+	t2.y = b2.y;
+	t2.z = b2.z + height3D;
+	
+	t3.x = b3.x;
+	t3.y = b3.y;
+	t3.z = b3.z + height3D;
+	
+	t4.x = b4.x;
+	t4.y = b4.y;
+	t4.z = b4.z + height3D;
+
+	Vec2 b1_2, b2_2, b3_2, b4_2, t1_2, t2_2, t3_2, t4_2;
+
+	if (ESPHack->WorldToScreen(b1, b1_2) && ESPHack->WorldToScreen(b2, b2_2)
+		&& ESPHack->WorldToScreen(b3, b3_2) && ESPHack->WorldToScreen(b4, b4_2)
+		&& ESPHack->WorldToScreen(t1, t1_2) && ESPHack->WorldToScreen(t2, t2_2)
+		&& ESPHack->WorldToScreen(t3, t3_2) && ESPHack->WorldToScreen(t4, t4_2)) {
+
+		// col
+		DrawLine(t1_2, b1_2, thickness, color);
+		DrawLine(t2_2, b2_2, thickness, color);
+		DrawLine(t3_2, b3_2, thickness, color);
+		DrawLine(t4_2, b4_2, thickness, color);
+
+		// top base
+		DrawLine(t1_2, t2_2, thickness, color);
+		DrawLine(t2_2, t3_2, thickness, color);
+		DrawLine(t3_2, t4_2, thickness, color);
+		DrawLine(t4_2, t1_2, thickness, color);
+		
+		// bottom base
+		DrawLine(b1_2, b2_2, thickness, color);
+		DrawLine(b2_2, b3_2, thickness, color);
+		DrawLine(b3_2, b4_2, thickness, color);
+		DrawLine(b4_2, b1_2, thickness, color);
+	}
+}
+
+void DrawText2D(const char* text, float x, float y, D3DCOLOR color, int alignment) {
+	RECT rect;
+
+	if (!ESPHack->Font)
+		D3DXCreateFont(pDevice, 14, 0, FW_NORMAL, 1, false, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Consolas", &ESPHack->Font);
+
+	SetRect(&rect, x + 1, y + 1, x + 1, y + 1);
+	ESPHack->Font->DrawTextA(NULL, text, -1, &rect, alignment | DT_NOCLIP, D3DCOLOR_ARGB(255,0,0,0));
+	
+	SetRect(&rect, x, y, x, y);
+	ESPHack->Font->DrawTextA(NULL, text, -1, &rect, alignment | DT_NOCLIP, color);
+
 }
--- CSGO Ruby/Drawing.h
+++ CSGO Ruby/Drawing.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "dx.h"
 #include "Vector.h"
-#include "Values.h"
 
 #define ABS(x) ((x < 0) ? (-x) : (x))
+#define TORAD(x) ((x) * static_cast<float>(0.01745329252))
 
 extern ID3DXLine* LineL;
 
@@ -13,3 +13,7 @@ void DrawLine(float x1, float y1, float x2, float y2, FLOAT thickness, D3DCOLOR 
 void DrawLine(Vec2 src, Vec2 dst, FLOAT thickness, D3DCOLOR color);
 
 void DrawESPBox2D(Vec2 top, Vec2 bot, FLOAT thickness, D3DCOLOR color);
+
+void DrawESPBox3D(Vec3 top, Vec3 bot, FLOAT a, int width, FLOAT thickness, D3DCOLOR color);
+
+void DrawText2D(const char* text, float x, float y, D3DCOLOR color, int alignment = DT_CENTER);
